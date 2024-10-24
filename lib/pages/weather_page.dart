5313b0a91a54ec367f4b0f8d5ba773cebe5d90c4
Dev Karan Singh <dev1604karan@gmail.com>
2024-10-24 17:21:27 +0530

    permissions added

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,16 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:mausam/models/weather_model.dart';
+import 'package:mausam/services/weather_service.dart';
 
-class WeatherPage extends StatelessWidget {
+class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  //api key
+  final _weatherService = WeatherService('f8dda0e84891cdc0f8ee1d0f82c072cb');
+  Weather? _weather;
+
+  //fetch weather
+  _fetchWeather() async {
+    //get current city
+    String cityName = await _weatherService.getCurrentCity();
+    print("City name: $cityName");
+    if (cityName == 'Unknown') {
+      print("Could not determine the city.");
+      return;
+    }
+
+    //get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+    //any errors
+    catch (e) {
+      print(e);
+    }
+  }
+
+  //weather animations
+
+  //init state
+  @override
+  void initState() {
+    super.initState();
+
+    //fetch weather on startup
+    _fetchWeather();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Weather'),
-      ),
-      body: const Center(
-        child: Text('Weather Page'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            //city name
+            Text(_weather?.cityName ?? "loading city..."),
+
+            //temperature
+            Text('${_weather?.temperature.round().toString()}0C'),
+          ],
+        ),
       ),
     );
   }
--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: deprecated_member_use
+// ignore_for_file: deprecated_member_use, constant_identifier_names
 
 import 'dart:convert';
 
@@ -10,16 +10,18 @@ import 'package:http/http.dart' as http;
 class WeatherService {
   static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
   final String apiKey;
+
   WeatherService(this.apiKey);
 
   Future<Weather> getWeather(String cityName) async {
     final response = await http
-        .get(Uri.parse('BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
+        .get(Uri.parse('$BASE_URL?q=$cityName&appid=$apiKey&units=metric'));
 
     if (response.statusCode == 200) {
       return Weather.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to load weather data');
+      throw Exception(
+          'Failed to load weather data. Status code: ${response.statusCode}');
     }
   }
 
